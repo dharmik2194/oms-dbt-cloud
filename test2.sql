3732de4139f6d4042913d9fd956d6d40a6ff8a75
Patel Dharmik Bipinchandra <pateldharmik2194@gmail.com>
2026-05-06 11:03:29 +0530

    Delete test2.sql

--- test2.sql
+++
@@ -1,9 +0,0 @@
-USE silver_db;
-CREATE TABLE customersNew (
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
-    nickname VARCHAR(100),
-    logo VARCHAR(40),
-    email VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
